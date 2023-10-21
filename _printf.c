7c331d10a137ee174e508f3cae85268faa03c4c5
qusaiisam <qusaiisam@gmail.com>
2023-10-21 07:22:06 -0700

    update

--- _printf.c
+++ _printf.c
@@ -4,30 +4,43 @@
  * _printf - implementation of the inbuilt printf
  * @format: the format specifier
  * Return: the formated string
- */
+*/
 
 int _printf(const char *format, ...)
 {
-	int printed = 0;
-
 	va_list args;
 
 	va_start(args, format);
 
-	while (*format != '\0')
+	int printed = 0, char *p = format;
+
+	while (*p != '\0')
 	{
-		if (*format == '%')
+		if (*p != '%')
 		{
-			format++;
-			printed = selector(format, args, printed);
-			format++;
+			_putchar(*p);
+			printed++;
 		}
 		else
 		{
-			_putchar(*format);
-			printed++;
-			format++;
+			if (*p == 'c')
+			{
+				_putchar(va_arg(args, int));
+				printed++;
+			}
+			else if (*p == 's')
+			{
+				printed = printf_string(args, printed);
+				printed++;
+				}
+			}
+			else if (*p == '%')
+			{
+				_putchar('%');
+				printed++;
+			}
 		}
+	p++;
 	}
 	va_end(args);
 	return (printed);
